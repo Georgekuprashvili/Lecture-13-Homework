ae00222713c64a4da5d3c8de7e880ae4821f6988
Georgekuprashvili <kuprashviligeorge@gmail.com>
2024-10-17 19:34:44 +0400

    H13

---
+++ script.js
@@ -0,0 +1,9 @@
+let name = " Giorgi ";
+let lastname = " Kuprashvili ";
+let age = " 22 ";
+let email = " kuprashviligeorge@gmail.com ";
+console.log(name + lastname + age + email);
+let mobilenumber = " 571048320 ";
+console.log("%c571048320", "color:red");
+let isheadult = true;
+console.log("%ctrue", "color:yellow");
